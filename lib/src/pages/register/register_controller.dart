11807218c4dae5e899860a352db43af1f26a0b12
mios260410 <mios260410@iMac-de-desarrollo.local>
2021-09-10 16:41:38 -0500

    Finalizar metodo para insertar un usuario usando la API

--- lib/src/pages/register/register_controller.dart
+++ lib/src/pages/register/register_controller.dart
@@ -1,4 +1,6 @@
+import 'package:delivery_alex_salcedo/src/models/response_api.dart';
 import 'package:delivery_alex_salcedo/src/models/user.dart';
+import 'package:delivery_alex_salcedo/src/provider/users_provider.dart';
 import 'package:flutter/material.dart';
 
 class RegisterController {
@@ -10,11 +12,14 @@ class RegisterController {
   TextEditingController passwordController = new TextEditingController();
   TextEditingController confirmPasswordController = new TextEditingController();
 
+  UsersProvider usersProvider = new UsersProvider();
+
   Future init(BuildContext context) {
     this.context = context;
+    usersProvider.init(context);
   }
 
-  void register() {
+  void register() async {
     String email = emailController.text.trim();
     String name = nameController.text.trim();
     String lastName = lastNameController.text.trim();
@@ -28,6 +33,10 @@ class RegisterController {
         lastname: lastName,
         phone: phone,
         password: password);
+
+    ResponseApi responseApi = await usersProvider.create(user);
+
+    print('RESPUESTA: ${responseApi.toJson()}');
 
     print('EMAIL: $email');
     print('NAME: $name');
