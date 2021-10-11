a96a64bf51b07394f9cb25db24e7423c4153b2cd
Alex Salcedo <alex_wss78@hotmail.com>
2021-10-11 18:38:11 -0500

    Creación de categoría desde Flutter

--- lib/src/pages/restaurant/categories/create/restaurant_categories_create_controller.dart
+++ lib/src/pages/restaurant/categories/create/restaurant_categories_create_controller.dart
@@ -1,4 +1,9 @@
+import 'package:delivery_alex_salcedo/src/models/category.dart';
+import 'package:delivery_alex_salcedo/src/models/response_api.dart';
+import 'package:delivery_alex_salcedo/src/models/user.dart';
+import 'package:delivery_alex_salcedo/src/provider/categories_provider.dart';
 import 'package:delivery_alex_salcedo/src/utils/my_snackbar.dart';
+import 'package:delivery_alex_salcedo/src/utils/shared_pref.dart';
 import 'package:flutter/material.dart';
 
 class RestaurantCategoriesCreateController {
@@ -8,18 +13,40 @@ class RestaurantCategoriesCreateController {
   TextEditingController nameController = new TextEditingController();
   TextEditingController descriptionController = new TextEditingController();
 
-  Future init(BuildContext context, Function refresh) {
+  CategoriesProvider _categoriesProvider = new CategoriesProvider();
+  User user;
+  SharedPref sharedPref = new SharedPref();
+
+  Future init(BuildContext context, Function refresh) async {
     this.context = context;
     this.refresh = refresh;
+
+    // Obtener el usuario almacenado en las preferencias de usuario
+    user = User.fromJson(await sharedPref.read('user'));
+
+    _categoriesProvider.init(context, user);
   }
 
-  void createCategory() {
+  void createCategory() async {
     String name = nameController.text;
     String description = descriptionController.text;
 
+    // Validación de ls datos
     if (name.isEmpty || description.isEmpty) {
       MySnackBar.show(context, 'Debe ingresar todos los campos');
       return;
+    }
+
+    Category category = new Category(name: name, description: description);
+
+    ResponseApi responseApi = await _categoriesProvider.create(category);
+
+    MySnackBar.show(context, responseApi.message);
+
+    if (responseApi.success) {
+      // Limpiar el formulario
+      nameController.text = '';
+      descriptionController.text = '';
     }
 
     print('Nombre: $name');
---
+++ lib/src/provider/categories_provider.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:delivery_alex_salcedo/src/api/environment.dart';
+import 'package:delivery_alex_salcedo/src/models/category.dart';
+import 'package:delivery_alex_salcedo/src/models/response_api.dart';
+import 'package:delivery_alex_salcedo/src/models/user.dart';
+import 'package:delivery_alex_salcedo/src/utils/shared_pref.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+
+class CategoriesProvider {
+  String _url = Environment.API_DELIVERY;
+  String _api = '/api/categories';
+  BuildContext context;
+  User sessionUser;
+
+  Future init(BuildContext context, User sessionUser) {
+    this.context = context;
+    this.sessionUser = sessionUser;
+  }
+
+  Future<ResponseApi> create(Category category) async {
+    try {
+      Uri url = Uri.http(_url, '$_api/create');
+      String bodyParams = json.encode(category);
+
+      Map<String, String> headers = {
+        'Content-type': 'application/json',
+        'Authorization': sessionUser.sessionToken //Asegurar la consulta
+      };
+
+      final res = await http.post(url, headers: headers, body: bodyParams);
+
+      if (res.statusCode == 401) {
+        Fluttertoast.showToast(msg: 'La sesión expiró');
+        new SharedPref().logout(context, sessionUser.id);
+      }
+
+      final data = json.decode(res.body);
+      ResponseApi responseApi = ResponseApi.fromJson(data);
+      return responseApi;
+    } catch (e) {
+      print('Error: $e');
+      return null;
+    }
+  }
+}
